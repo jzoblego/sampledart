8aae4fb67db1f6e6c3c4da6fcfb8e30e6236755f
jeffrey oblego <jzoblego@gmail.com>
2023-06-29 21:55:44 +0800

    First Commit

---
+++ bin/pjdart_sample01.dart
@@ -0,0 +1,11 @@
+import 'package:pjdart_sample01/pjdart_sample01.dart' as pjdart_sample01;
+import 'dart:io';
+
+void main(){
+
+
+// void main(List<String> arguments) {
+  // print('Hello world: ${pjdart_sample01.calculate()}!');
+  print('Hello world: ');
+  string username = stdin.readLineSync();
+}
---
+++ test/pjdart_sample01_test.dart
@@ -0,0 +1,8 @@
+import 'package:pjdart_sample01/pjdart_sample01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
